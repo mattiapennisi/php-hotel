501c97d2145339474770c4cc9f3c30e7f1aece3d
mattiapennisi <mattiapennisi0@gmail.com>
2025-06-18 12:45:43 +0200

    Enhance hotel filtering functionality by adding parking and rating filters to the form

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+// Array containing hotel data
 $hotels = [
 
     [
@@ -40,7 +41,9 @@ $hotels = [
 
 ];
 
+// Check if the parking filter is set in the GET request
 $parkingGetQueryExists = isset($_GET['filterByParking']);
+// Get the rating filter value from GET request, default to 1 if not set
 $ratingFilterValue = isset($_GET['filterByRating']) ? (int)$_GET['filterByRating'] : 1;
 
 ?>
@@ -52,6 +55,7 @@ $ratingFilterValue = isset($_GET['filterByRating']) ? (int)$_GET['filterByRating
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotels</title>
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 
@@ -62,11 +66,14 @@ $ratingFilterValue = isset($_GET['filterByRating']) ? (int)$_GET['filterByRating
         <h1 class="text-center mt-4"><a href='./index.php'>Hotels List</a></h1>
 
         <h5 class="mt-4">Filters:</h5>
+        <!-- Filter form -->
         <form class="d-flex flex-row form-control" action="./index.php" method="get">
 
+            <!-- Parking filter checkbox -->
             <input class="me-2" type="checkbox" name="filterByParking">
             <label class="me-4 pt-1" for="filterByParking">Only with parking</label>
 
+            <!-- Rating filter select -->
             <select class="me-4" name="filterByRating">
                 <option>Filter by rating</option>
                 <option value="1">One star</option>
@@ -76,12 +83,14 @@ $ratingFilterValue = isset($_GET['filterByRating']) ? (int)$_GET['filterByRating
                 <option value="5">Five stars</option>
             </select>
 
+            <!-- Submit button -->
             <button type="submit" class="btn btn-outline-primary">Enter</button>
 
         </form>
 
         <?php
 
+        // Start table
         echo "<table class='table mt-4'>
             <thead>
                 <tr>
@@ -95,8 +104,10 @@ $ratingFilterValue = isset($_GET['filterByRating']) ? (int)$_GET['filterByRating
             <tbody>
         ";
 
+        // Loop through each hotel and apply filters
         foreach ($hotels as $hotel) {
 
+            // If parking filter is set, show only hotels with parking and matching rating
             if ($parkingGetQueryExists && $hotel['parking'] && $hotel['vote'] >= $ratingFilterValue) {
                 echo "
                     <tr>
@@ -107,6 +118,7 @@ $ratingFilterValue = isset($_GET['filterByRating']) ? (int)$_GET['filterByRating
                         <td>{$hotel['distance_to_center']} Km</td>
                     </tr>
                     ";
+                // If parking filter is not set, show all hotels matching the rating
             } else if (!$parkingGetQueryExists && $hotel['vote'] >= $ratingFilterValue) {
                 echo "
                     <tr>
@@ -120,6 +132,7 @@ $ratingFilterValue = isset($_GET['filterByRating']) ? (int)$_GET['filterByRating
             }
         }
 
+        // End table
         echo "
             </tbody>
         </table>
@@ -129,6 +142,7 @@ $ratingFilterValue = isset($_GET['filterByRating']) ? (int)$_GET['filterByRating
 
     </div>
 
+    <!-- Bootstrap JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 
 </body>
